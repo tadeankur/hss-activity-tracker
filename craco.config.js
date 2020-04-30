c7c9d6a7730ff0e341952ddee4e99d37d6d0a826
ankur <H3rl3k1r123>
2020-04-30 22:13:46 +0200

    updted craco config file for build

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+const { ESLINT_MODES } = require('@craco/craco');
+const CracoLessPlugin = require('craco-less');
 
-}
+const DisableAsciiOnly = {
+    overrideWebpackConfig: ({ webpackConfig, context: { env } }) => {
+        if (env === 'production') {
+            webpackConfig.optimization.minimizer[0].options.terserOptions.output.ascii_only = false;
+        }
+        return webpackConfig;
+    }
+};
+
+module.exports = function({ env }) {
+    return {
+        eslint: {
+            mode: ESLINT_MODES.file
+        },
+        plugins: [{ plugin: DisableAsciiOnly }, { plugin: CracoLessPlugin }]
+    };
+};
